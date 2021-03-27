a02ed6490c21ce142a06064e247fb13d37451238
GC <12754600+genesaturn@users.noreply.github.com>
2021-03-27 11:36:14 +0800

    path changed

--- app.js
+++
@@ -1,9 +0,0 @@
-function share(shareTitle, shareText, shareUrl){
-    if (navigator.share) {
-        navigator.share({
-          title: shareTitle,
-          text: shareText,
-          url: shareUrl,
-        })
-      }
-}
